aa8a6cc75d6f18b9ce3aa87eb95a569708fbaae7
Alexei Nasra <alexinasra@gmail.com>
2026-02-21 19:38:49 +0200

    remove cached files from repo

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#77cbe0", "#000101" },
-	[SchemeSel] = { "#77cbe0", "#00637C" },
-	[SchemeOut] = { "#77cbe0", "#009CC4" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#000101";
-static const char* selfgcolor   = "#77cbe0";
-static const char* normbgcolor  = "#00718E";
-static const char* normfgcolor  = "#77cbe0";
-static const char* urgbgcolor   = "#00637C";
-static const char* urgfgcolor   = "#77cbe0";
