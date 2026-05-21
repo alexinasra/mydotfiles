facf0cfa8f1c1e29736fe695a88709ddfee148aa
Alexei Nasra <alexinasra@gmail.com>
2026-05-22 00:40:36 +0300

    setup workspaces per monitor

--- .config/hypr/lua/workspaces.lua
+++ .config/hypr/lua/workspaces.lua
@@ -47,3 +47,15 @@ hl.window_rule({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+
+hl.workspace_rule({ workspace = "1",   monitor = "eDP-1", default = true})
+hl.workspace_rule({ workspace = "2",   monitor = "eDP-1"})
+hl.workspace_rule({ workspace = "3",   monitor = "eDP-1"})
+hl.workspace_rule({ workspace = "4",   monitor = "eDP-1"})
+hl.workspace_rule({ workspace = "5",   monitor = "eDP-1"})
+hl.workspace_rule({ workspace = "6",   monitor = "eDP-1"})
+hl.workspace_rule({ workspace = "7",   monitor = "HDMI-A-1"})
+hl.workspace_rule({ workspace = "8",   monitor = "HDMI-A-1"})
+hl.workspace_rule({ workspace = "9",   monitor = "HDMI-A-1"})
+hl.workspace_rule({ workspace = "10",  monitor = "HDMI-A-1", default = true})
