9080bd3e552027169e3a5e66316e347d0c9bad99
Alexei Nasra <alexinasra@gmail.com>
2026-05-22 00:07:32 +0300

    fixed qt theme env variable

--- .config/hypr/lua/enviroment_variables.lua
+++ .config/hypr/lua/enviroment_variables.lua
@@ -16,6 +16,7 @@ hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_THEME", "Empty-Butterfly-Purpl")
 
 --- QT ---
-hl.env("QT_QPA_PLATFORM", "gtk3")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_QPA_PLATFORMTHEME", "gtk3")
 
 
