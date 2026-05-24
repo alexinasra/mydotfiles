3a3f0c6afa213953c28b00aa10cb5272883c7cdf
Alexei Nasra <alexinasra@gmail.com>
2026-05-24 20:17:50 +0300

    globals file

---
+++ .config/hypr/globals.lua
@@ -0,0 +1,27 @@
+-- Cursor settings
+CURSOR_THEME  = "Empty-Butterfly-Purpl"
+CURSOR_SIZE   = "24"
+
+
+-- Set programs that you use
+TERMINAL = "wezterm"
+FILEMANAGER = "cosmic-files"
+MENU        = "hyprlauncher"
+EDITOR      = "appscratcheditor"
+BROWSER     = "firefox"
+CALCULATOR  = "qalculate-gtk"
+
+
+-- QT Settings
+QT_QPA_PLATFORMTHEME = "gtk3"
+
+
+-- Look and Feel
+GENERAL_GAPS_IN = 5
+GENERAL_GAPS_OUT = 2
+GENERAL_BORDER_SIZE = 1
+GENERAL_RESIZE_ONBORDER = true
+GENERAL_DECORATION_ROUNDING = 4
+GENERAL_DECORATION_ROUNDING_POWER = 2
+GENERAL_ANIMATIONS_ENABLED = true
+GENERAL_LAYOUT = "scrolling"
