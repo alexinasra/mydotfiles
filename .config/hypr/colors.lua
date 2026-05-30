974213a148ee12676487468890f9c173476a2e95
Alexei Nasra <alexinasra@gmail.com>
2026-05-30 18:54:45 +0300

    commit current state

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,7 +1,7 @@
 -- Colors
 -- Generated with wal-tile (see ~/.config/wal/template and ~/.bashrc.d/wal.sh)
 COLORS = {
-  COL_ACTIVE_BORDER  = { 0xFFC13E34, 0xCCDD5958 },
-  COL_INACTIVE_BORDER = 0xFF739090,
-  DECORATION_SHADOW   = 0x33c2cacb
+  COL_ACTIVE_BORDER  = { 0xFF2B9560, 0xCC5F9B65 },
+  COL_INACTIVE_BORDER = 0xFF337987,
+  DECORATION_SHADOW   = 0x339cdbbf
 }
