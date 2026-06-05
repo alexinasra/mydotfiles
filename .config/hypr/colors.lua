046f6e411ea951eb2e8ccd68a82c10845fa656a9
Alexei Nasra <alexinasra@gmail.com>
2026-06-05 21:02:08 +0300

    theme update

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,7 +1,7 @@
 -- Colors
 -- Generated with wal-tile (see ~/.config/wal/template and ~/.bashrc.d/wal.sh)
 COLORS = {
-  COL_ACTIVE_BORDER  = { 0xFF2B9560, 0xCC5F9B65 },
-  COL_INACTIVE_BORDER = 0xFF337987,
-  DECORATION_SHADOW   = 0x339cdbbf
+  COL_ACTIVE_BORDER  = { 0xFFBFC028, 0xCC553E8A },
+  COL_INACTIVE_BORDER = 0xFF7A8492,
+  DECORATION_SHADOW   = 0x33bdbec2
 }
