ebc01b4333cf4a4509fd24b09bd9a4d73776da0e
Alexei Nasra <alexinasra@gmail.com>
2026-06-08 22:31:15 +0300

    watamelon theme

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,7 +1,7 @@
 -- Colors
 -- Generated with wal-tile (see ~/.config/wal/template and ~/.bashrc.d/wal.sh)
 COLORS = {
-  COL_ACTIVE_BORDER  = { 0xFF735723, 0xCC936607 },
-  COL_INACTIVE_BORDER = 0xFFe7c07e,
-  DECORATION_SHADOW   = 0x33C59B1E
+  COL_ACTIVE_BORDER  = { 0xFF62916C, 0xCC84C45E },
+  COL_INACTIVE_BORDER = 0xFF84C45E,
+  DECORATION_SHADOW   = 0x33EED5D2
 }
