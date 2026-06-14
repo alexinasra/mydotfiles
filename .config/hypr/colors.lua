45fd979147f493f0142926fbc970522b32d55535
Alexei Nasra <alexinasra@gmail.com>
2026-06-14 16:30:44 +0300

    sandy theme

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,7 +1,7 @@
 -- Colors
 -- Generated with wal-tile (see ~/.config/wal/template and ~/.bashrc.d/wal.sh)
 COLORS = {
-  COL_ACTIVE_BORDER  = { 0xFF62916C, 0xCC84C45E },
-  COL_INACTIVE_BORDER = 0xFF84C45E,
-  DECORATION_SHADOW   = 0x33EED5D2
+  COL_ACTIVE_BORDER  = { 0xFFC38B5C, 0xCCD0A57A },
+  COL_INACTIVE_BORDER = 0xFFD0A57A,
+  DECORATION_SHADOW   = 0x33E1B78D
 }
